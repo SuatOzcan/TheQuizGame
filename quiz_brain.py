9fc65efe9aed269146c7cafb119a1411fd7e2eda
SuatOzcan <41187131+SuatOzcan@users.noreply.github.com>
2023-11-08 16:48:04 +0300

    Shuffle the question list before presenting it to the player.

--- quiz_brain.py
+++ quiz_brain.py
@@ -4,16 +4,18 @@ import random
 class QuizBrain():
     def __init__(self):
         self.question_number = 0
-        self.question_list = question_list
         self.player_score = 0
+        self.question_list = question_list
+        random.shuffle(self.question_list)
 
     def next_question(self):
         for i in range(len(self.question_list)):
-            random_pick = self.question_list[random.randrange(0,len(question_list))]
+            random_pick = self.question_list[i]
             user_input = input(random_pick.question + ' True\False\n')
             if user_input == random_pick.answer:
                 self.player_score += 1
             print(f'Your score is {player_question.player_score}.')
         print('Game over.')
+
 player_question = QuizBrain()
 player_question.next_question()
