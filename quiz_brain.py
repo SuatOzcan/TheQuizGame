d818592a002c75fa8da55d25d20df0a96cb7e416
SuatOzcan <41187131+SuatOzcan@users.noreply.github.com>
2023-11-08 17:44:37 +0300

    Display the answered question number.

--- quiz_brain.py
+++ quiz_brain.py
@@ -17,8 +17,11 @@ class QuizBrain():
         random_pick = self.question_list[self.question_number]
         self.question_number += 1
         user_input = input(f'Q.{self.question_number}: ' + random_pick.question + ' True\False\n')
-        if user_input == random_pick.answer:
-            print(f"You've got it right!. Your point is: {self.player_score} \ {len(self.question_list)}")
+        self.user_answer(random_pick, user_input)
+
+    def user_answer(self, random_pick, user_input):
+        if user_input.lower() == random_pick.answer.lower():
             self.player_score += 1
+            print(f"You've got it right!. Your point is: {self.player_score} \ {self.question_number}")            
         else:
-            print(f"The answer is not correct. No worries! Your point is: {self.player_score} \ {len(self.question_list)}")
+            print(f"The answer is not correct. No worries! Your point is: {self.player_score} \ {self.question_number}")
